637868ed4dee64716396295cb9047d96a73d5138
Shobhit <hathishobhit@gmail.com>
2018-10-09 23:20:45 -0700

    started vanilla autoencoder

---
+++ autoencoder/autoencoder_model.py
@@ -0,0 +1,25 @@
+from torch import nn as nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST
+
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.encoder = nn.Sequential(nn.Linear(28 * 28, 128),
+                                     nn.ReLU(True),
+                                     nn.Linear(128, 64),
+                                     nn.ReLU(True),
+                                     nn.Linear(64, 12))
+        self.decoder = nn.Sequential(nn.Linear(12, 64), nn.ReLU(True),
+                                     nn.Linear(64, 128),
+                                     nn.ReLU(True),
+                                     nn.Linear(128, 28*28),
+                                     nn.Tanh())
+
+    def forward(self, x):
+        encoded_y = self.encoder(x)
+        y = self.decoder(encoded_y)
+
